c3f3080d183f5ee27d83199171ee2e4c9c10a240
yiyan <yiyan.ng.ml@gmail.com>
2026-08-28 23:28:52 +0100

    chore: export all environments and estimators

--- src/agent_credit_bench/envs/__init__.py
+++ src/agent_credit_bench/envs/__init__.py
@@ -1,5 +1,15 @@
 """Diagnostic environments (plan.md §8)."""
 
 from agent_credit_bench.envs.delayed_effect import DelayedEffectEnv
+from agent_credit_bench.envs.recovery import RecoveryEnv
+from agent_credit_bench.envs.variable_horizon import (
+    StopProbabilityPolicy,
+    VariableHorizonEnv,
+)
 
-__all__ = ["DelayedEffectEnv"]
+__all__ = [
+    "DelayedEffectEnv",
+    "RecoveryEnv",
+    "StopProbabilityPolicy",
+    "VariableHorizonEnv",
+]
--- src/agent_credit_bench/estimators/__init__.py
+++ src/agent_credit_bench/estimators/__init__.py
@@ -4,6 +4,7 @@ from agent_credit_bench.estimators.base import CreditEstimator, EstimatorContext
 from agent_credit_bench.estimators.batch_centered import BatchCenteredBroadcast
 from agent_credit_bench.estimators.oracle import OracleAdvantage
 from agent_credit_bench.estimators.outcome_broadcast import OutcomeBroadcast
+from agent_credit_bench.estimators.turn_loo import TurnLOO
 
 __all__ = [
     "BatchCenteredBroadcast",
@@ -11,4 +12,5 @@ __all__ = [
     "EstimatorContext",
     "OracleAdvantage",
     "OutcomeBroadcast",
+    "TurnLOO",
 ]
